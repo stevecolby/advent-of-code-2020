21d39907537bf07514ff0844392b4e4a2b438d39
Steve Colby <stevecolby@gmail.com>
2020-12-02 22:08:59 -0500

    day2 part 2 complete

--- day2.py
+++ day2.py
@@ -11,16 +11,25 @@ def main():
 
 def isPasswordValid(input_line):
     line_elements = input_line.strip('\n').replace(':', '').split(' ')
-    min_count = int(line_elements[0].split('-')[0])
-    max_count = int(line_elements[0].split('-')[1])
+    first_location = int(line_elements[0].split('-')[0])-1
+    second_location = int(line_elements[0].split('-')[1])-1
     searchCharacter = line_elements[1]
     password = line_elements[2]
 
-    numCharsFound = password.count(searchCharacter)
+    matchCount = 0
 
-    if numCharsFound >= min_count and numCharsFound <= max_count :
-        print(numCharsFound, ' is between ', min_count, ' and ', max_count)
+    if password[first_location] == searchCharacter:
+        matchCount += 1
+
+    if password[second_location] == searchCharacter:
+        matchCount += 1
+
+    if matchCount == 1:
+        print('MATCH:', input_line)
         return True
+
+    elif matchCount == 2:
+        print('DOUBLE MATCH: ', input_line)
     
     return False
 

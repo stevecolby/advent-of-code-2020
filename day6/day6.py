7b3744a9509d269208372de6e0e706885fd9f176
Steve Colby <stevecolby@gmail.com>
2020-12-06 21:29:33 -0500

    day 6 part 2

--- day6/day6.py
+++ day6/day6.py
@@ -3,20 +3,30 @@ def Day6():
 
     with open('day6/day6_input') as input_file:
         lines = input_file.readlines()
+        first_of_group = True
 
         current_group = []
 
         for line in lines:
             line = line.strip('\n')
 
-            if line == '':
+            if first_of_group:
                 running_total += len(current_group)
-                current_group = []
+                current_group = list(line)
+                first_of_group = False
+
+            elif line == '':
+                first_of_group = True
+            
             else:
-                for answer in list(line):
-                    if answer not in current_group:
-                        current_group.append(answer)
-                print('')
+                current_line = list(line)
+                removed_items = []
+                for item in current_group:
+                    if item not in current_line:
+                        removed_items.append(item)
+                
+                for item in removed_items:
+                    current_group.remove(item)
         
 
         if len(current_group):

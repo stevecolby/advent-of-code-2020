b60916ebf24342f4db4d5a25e79b8ea0372657eb
Steve Colby <stevecolby@gmail.com>
2020-12-08 09:14:14 -0500

    changed main and refactored day 7 to calculate and call days dynamically based on a day number

--- day7/day7.py
+++ day7/day7.py
@@ -8,37 +8,37 @@ class Bag(object):
         self.contents = []
         self.count = 0
 
-def Day7():
+def Day7(lines):
     bags = []
 
-    with open('day7/day7_input') as input_file:
-        lines = input_file.readlines()
+    #with open('day7/day7_input') as input_file:
+    #lines = input_file.readlines()
 
-        for line in lines:
-            line = line.strip('\n.').replace(',', '').replace('bags', 'bag')+' '
+    for line in lines:
+        line = line.strip('\n.').replace(',', '').replace('bags', 'bag')+' '
 
-            #striped tan bags contain 2 light silver bags, 1 drab black bag, 2 clear tan bags, 2 mirrored tan bags.
-            main_bag = Bag()
-            main_bag.color = line.split(' bag contain ')[0].replace(' ', '')
-            main_bag.count = 1
+        #striped tan bags contain 2 light silver bags, 1 drab black bag, 2 clear tan bags, 2 mirrored tan bags.
+        main_bag = Bag()
+        main_bag.color = line.split(' bag contain ')[0].replace(' ', '')
+        main_bag.count = 1
 
-            rest_of_line = line.split(' bag contain ')[1].split()
+        rest_of_line = line.split(' bag contain ')[1].split()
 
-            if 'other' not in rest_of_line:
-                content_bag = Bag()
-                #2 light silver bag 1 drab black bag 2 clear tan bag 2 mirrored tan bag
-                for item in rest_of_line:
-                    if item == 'bag':
-                        main_bag.contents.append(content_bag)
-                        content_bag = Bag()
-                    elif item.isdigit():
-                        content_bag.count = int(item)
-                    else:
-                        content_bag.color += item
-            else:
-                print('')
-            
-            bags.append(main_bag)
+        if 'other' not in rest_of_line:
+            content_bag = Bag()
+            #2 light silver bag 1 drab black bag 2 clear tan bag 2 mirrored tan bag
+            for item in rest_of_line:
+                if item == 'bag':
+                    main_bag.contents.append(content_bag)
+                    content_bag = Bag()
+                elif item.isdigit():
+                    content_bag.count = int(item)
+                else:
+                    content_bag.color += item
+        else:
+            print('')
+        
+        bags.append(main_bag)
     total_bag_count = 0
 
     shiny_gold_bag = [x for x in bags if x.color == 'shinygold'][0]

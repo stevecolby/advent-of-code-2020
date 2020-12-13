5c03c4708e5443dde6823f769fa70e3533c23669
Steve Colby <stevecolby@gmail.com>
2020-12-13 17:44:45 -0500

    day 13.2 done

--- day13/day13.py
+++ day13/day13.py
@@ -1,19 +1,36 @@
 def Run(lines):
-    earliest_time = int(lines[0])
     active_bus_list = []
+    increment_amount = 0
+    bus_list = lines[1].split(',')
 
-    for bus in lines[1].split(','):
-        if bus is not 'x':
-            active_bus_list.append(int(bus))
+    x = 0
+    while x < len(bus_list):
+        if bus_list[x] != 'x':
+            bus_id = int(bus_list[x])
+            bus_and_offset = [bus_id, x]
+            active_bus_list.append(bus_and_offset)
+        x+=1
     
-    min_wait_time = 10000000
-    min_wait_bus_ID = 0
 
-    for bus in active_bus_list:
-        wait_time = ((1+int(earliest_time/bus))*bus) - earliest_time
+    iteration_amt = active_bus_list[0][0]
+    start = active_bus_list[0][0]
+    
+    for bus in active_bus_list[1:]:
+        start = get_next_departure(start, iteration_amt, bus)
+        iteration_amt = iteration_amt * int(bus[0])
+
+    print(start)
+
+def get_next_departure(start, iteration, bus):
+    current_departure_counter = start
+    found = False
+
+    while not found:        
+        if ((current_departure_counter + bus[1]) % bus[0]) == 0:
+            #match
+            return current_departure_counter
         
-        if wait_time < min_wait_time:
-            min_wait_time = wait_time
-            min_wait_bus_ID = bus
-    
-    print(min_wait_time * min_wait_bus_ID)
+        current_departure_counter += iteration
+                
+        #if earliest_time_counter > 1068700:
+        #print(current_departure_counter)
